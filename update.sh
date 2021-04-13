c1608c680bbc8b25027b18273a82ae252ab6db40
didi <git@d10r.net>
2021-04-13 22:29:20 +0200

    give overlay precedence in deduplication

--- update.sh
+++ update.sh
@@ -18,7 +18,7 @@ ethSource2="https://tokens.coingecko.com/uniswap/all.json"
 curl -f -s $ethSource1 | jq '.tokens' > eth1.json
 curl -f -s $ethSource2 | jq '.tokens' > eth2.json
 # concat into one file
-jq -s '.|flatten' eth1.json eth2.json lab10_eth_overlay.json > eth0.json
+jq -s '.|flatten' lab10_eth_overlay.json eth1.json eth2.json > eth0.json
 deduplicate eth0.json eth.json
 # cleanup
 rm -f eth?.json
@@ -31,7 +31,7 @@ xdaiSource1="https://raw.githubusercontent.com/1Hive/default-token-list/master/s
 # here jq just validates the content
 curl -f -s $xdaiSource1 | jq '.' > xdai1.json
 # concat into one file
-jq -s '.|flatten' xdai1.json lab10_xdai_overlay.json > xdai0.json
+jq -s '.|flatten' lab10_xdai_overlay.json xdai1.json > xdai0.json
 deduplicate xdai0.json xdai.json
 # cleanup
 rm -f xdai?.json
