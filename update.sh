09108507f559dfea5f4cd5ac1beefb2aaa73c4b8
PeterTheOne <petertheone@gmail.com>
2023-02-22 19:57:44 +0100

    make script more robust to curl and json parse errors

--- update.sh
+++ update.sh
@@ -20,10 +20,10 @@ ethSource2="https://api.coinmarketcap.com/data-api/v3/uniswap/all.json"
 ethSource3="https://tokens.coingecko.com/uniswap/all.json"
 ethSource4="https://t2crtokens.eth.link/"
 
-curl -f -s $ethSource1 | jq '.tokens' > eth1.json
-curl -f -s $ethSource2 | jq '.tokens' > eth2.json
-curl -f -s $ethSource3 | jq '.tokens' > eth3.json
-curl -f -s $ethSource4 | jq '.tokens' > eth4.json
+curl -f -s $ethSource1 | jq '.tokens' > eth1.json || true
+curl -f -s $ethSource2 | jq '.tokens' > eth2.json || true
+curl -f -s $ethSource3 | jq '.tokens' > eth3.json || true
+curl -f -s $ethSource4 | jq '.tokens' > eth4.json || true
 # concat into one file
 jq -s '.|flatten' lab10_eth_overlay.json eth1.json eth2.json eth3.json eth4.json > eth0.json
 deduplicate eth0.json eth.json
@@ -37,7 +37,7 @@ echo "built eth list with $(jq '.|length' eth.json) elements"
 xdaiSource1="https://raw.githubusercontent.com/1Hive/default-token-list/master/src/tokens/xdai.json"
 
 # here jq just validates the content
-curl -f -s $xdaiSource1 | jq '.' > xdai1.json
+curl -f -s $xdaiSource1 | jq '.' > xdai1.json || true
 # concat into one file
 jq -s '.|flatten' lab10_xdai_overlay.json xdai1.json > xdai0.json
 deduplicate xdai0.json xdai.json
@@ -50,7 +50,7 @@ echo "built xdai list with $(jq '.|length' xdai.json) elements"
 
 maticSource1="https://unpkg.com/quickswap-default-token-list/build/quickswap-default.tokenlist.json"
 
-curl -f -s -L $maticSource1 | jq '.tokens' > matic1.json
+curl -f -s -L $maticSource1 | jq '.tokens' > matic1.json || true
 jq -s '.|flatten' lab10_matic_overlay.json matic1.json > matic0.json
 deduplicate matic0.json matic.json
 rm -f matic?.json
@@ -60,32 +60,19 @@ echo "built matic list with $(jq '.|length' matic.json) elements"
 
 bscSource1="https://tokens.pancakeswap.finance/pancakeswap-extended.json"
 
-curl -f -s -L $bscSource1 | jq '.tokens' > bsc1.json
+curl -f -s -L $bscSource1 | jq '.tokens' > bsc1.json || true
 jq -s '.|flatten' lab10_bsc_overlay.json bsc1.json > bsc0.json
 deduplicate bsc0.json bsc.json
 rm -f bsc?.json
 echo "built bsc list with $(jq '.|length' bsc.json) elements"
 
-########### OKEX ############
-
-okexSource1="https://resources.jswap.finance/token-list/oec/extended.tokenlist.json"
-okexSource2="https://static.kswap.finance/tokenlist/kswap-hosted-list.json"
-
-curl -f -s $okexSource1 | jq '.tokens' > okex1.json
-curl -f -s $okexSource2 | jq '.tokens' > okex2.json
-# concat into one file
-jq -s '.|flatten' lab10_okex_overlay.json okex1.json okex2.json > okex0.json
-deduplicate okex0.json okex.json
-rm -f okex?.json
-echo "built okex list with $(jq '.|length' okex.json) elements"
-
 ########### ARBITRUM ############
 
 arbitrumSource1="https://raw.githubusercontent.com/sushiswap/list/master/lists/token-lists/default-token-list/tokens/arbitrum.json"
-#arbitrumSource2="https://bridge.arbitrum.io/token-list-42161.json"
+arbitrumSource2="https://bridge.arbitrum.io/token-list-42161.json"
 
-curl -f -s $arbitrumSource1 | jq '.' > arbitrum1.json
-#curl -f -s $arbitrumSource2 | jq '.tokens' > arbitrum2.json
+curl -f -s $arbitrumSource1 | jq '.' > arbitrum1.json || true
+curl -f -s $arbitrumSource2 | jq '.tokens' > arbitrum2.json || true
 # concat into one file
 jq -s '.|flatten' lab10_arbitrum_overlay.json arbitrum1.json arbitrum2.json > arbitrum0.json
 deduplicate arbitrum0.json arbitrum.json
@@ -98,8 +85,8 @@ optimismSource1="https://raw.githubusercontent.com/sushiswap/list/master/lists/t
 optimismSource2="https://static.optimism.io/optimism.tokenlist.json"
 
 
-curl -f -s $optimismSource1 | jq '.' > optimism1.json
-curl -f -s $optimismSource2 | jq '.tokens' > optimism2.json
+curl -f -s $optimismSource1 | jq '.' > optimism1.json || true
+curl -f -s $optimismSource2 | jq '.tokens' > optimism2.json || true
 # concat into one file
 jq -s '.|flatten' lab10_optimism_overlay.json optimism1.json optimism2.json > optimism0.json
 deduplicate optimism0.json optimism.json
@@ -111,8 +98,8 @@ echo "built optimism list with $(jq '.|length' optimism.json) elements"
 celoSource1="https://raw.githubusercontent.com/sushiswap/list/master/lists/token-lists/default-token-list/tokens/celo.json"
 celoSource2="https://raw.githubusercontent.com/Ubeswap/default-token-list/master/ubeswap.token-list.json"
 
-curl -f -s $celoSource1 | jq '.' > celo1.json
-curl -f -s $celoSource2 | jq '.tokens' > celo2.json
+curl -f -s $celoSource1 | jq '.' > celo1.json || true
+curl -f -s $celoSource2 | jq '.tokens' > celo2.json || true
 # concat into one file
 jq -s '.|flatten' lab10_celo_overlay.json celo1.json celo2.json > celo0.json
 deduplicate celo0.json celo.json
@@ -125,9 +112,9 @@ avalancheSource1="https://raw.githubusercontent.com/sushiswap/list/master/lists/
 avalancheSource2="https://raw.githubusercontent.com/traderjoe-xyz/joe-tokenlists/main/joe.tokenlist.json"
 avalancheSource3="https://raw.githubusercontent.com/pangolindex/tokenlists/main/pangolin.tokenlist.json"
 
-curl -f -s $avalancheSource1 | jq '.' > avalanche1.json
-curl -f -s $avalancheSource2 | jq '.tokens' > avalanche2.json
-curl -f -s $avalancheSource3 | jq '.tokens' > avalanche3.json
+curl -f -s $avalancheSource1 | jq '.' > avalanche1.json || true
+curl -f -s $avalancheSource2 | jq '.tokens' > avalanche2.json || true
+curl -f -s $avalancheSource3 | jq '.tokens' > avalanche3.json || true
 # concat into one file
 jq -s '.|flatten' lab10_avalanche_overlay.json avalanche1.json avalanche2.json avalanche3.json > avalanche0.json
 deduplicate avalanche0.json avalanche.json
