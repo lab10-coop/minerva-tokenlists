5368784c55a861f9da6607ef48232f152d827645
Peter Grassberger <petertheone@gmail.com>
2023-10-13 12:03:13 +0200

    add superfluid.tokenlist.json

--- update.sh
+++ update.sh
@@ -144,9 +144,23 @@ echo "validated sigma1 list with $(jq '.|length' sigma1.json) elements"
 # the testnets list is static for now
 echo "validated kovan list with $(jq '.|length' testnets.json) elements"
 
+########### NON-SPECIFIC NETWORK ############
+
+nonSpecificSource1="https://raw.githubusercontent.com/superfluid-finance/tokenlist/main/superfluid.tokenlist.json"
+
+# here jq just validates the content
+curl -f -s $nonSpecificSource1 | jq '.tokens' > nonspecific1.json || true
+# concat into one file
+jq -s '.|flatten' lab10_nonspecific_overlay.json nonspecific1.json > nonspecific0.json
+deduplicate nonspecific0.json nonspecific.json
+# cleanup
+rm -f nonspecific?.json
+
+echo "built non-specific network list with $(jq '.|length' nonspecific.json) elements"
+
 ########### merge all ###########
 
 # compile a multi-network list
-jq -s '.|flatten' eth.json xdai.json sigma1.json matic.json bsc.json okex.json arbitrum.json optimism.json celo.json avalanche.json zksync.json testnets.json > all.json
+jq -s '.|flatten' eth.json xdai.json sigma1.json matic.json bsc.json okex.json arbitrum.json optimism.json celo.json avalanche.json zksync.json testnets.json nonspecific.json > all.json
 
 echo "built all list with $(jq '.|length' all.json) elements"
